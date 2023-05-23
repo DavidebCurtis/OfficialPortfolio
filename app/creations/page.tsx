7ae1cc017e4572bbe5ab5a1dbca37a6b751a69f8
david curtis <heythereimdavidcurtis@gmail.com>
2023-05-23 13:11:09 -0400

    added projects

--- app/creations/page.tsx
+++ app/creations/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
-import weather from '../../public/images/quiz.png';
+import weather from '../../public/images/weather.png';
+import virtualgarden from '../../public/images/virtualgarden.png';
+import anime from '../../public/images/quiz.png';
+import taskmaster from '../../public/images/taskmaster.png';
+import placeholder from '../../public/images/placeholder.png';
 
 const projects = () => {
   return (
@@ -36,73 +40,85 @@ const projects = () => {
               placeholder='blur'
               loading='lazy'
             />
-            <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
-              <a
-                target='_blank'
-                href='https://davidebcurtis.github.io/weather-dashboard/'
-              >
+            <a
+              target='_blank'
+              href='https://davidebcurtis.github.io/weather-dashboard/'
+            >
+              <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
                 Weather App
-              </a>
+              </div>
+            </a>
+          </div>
+          <div className='relative absolute inset-0 cursor-pointer'>
+            <Image
+              className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
+              src={virtualgarden}
+              alt='Virtual Garden'
+              placeholder='blur'
+              loading='lazy'
+            />
+            <a href='https://plantgardenforum.herokuapp.com/' target='_blank'>
+              <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
+                Virtual Garden
+              </div>
+            </a>
+          </div>
+          <div className='relative absolute inset-0 cursor-pointer'>
+            <Image
+              className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
+              src={anime}
+              alt='Anime Quiz'
+              placeholder='blur'
+              loading='lazy'
+            />
+            <a
+              target='_blank'
+              href='https://davidebcurtis.github.io/anime-quiz/'
+            >
+              <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
+                Anime Quiz
+              </div>
+            </a>
+          </div>
+          <div className='relative absolute inset-0 cursor-pointer'>
+            <Image
+              className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
+              src={taskmaster}
+              alt='Task Master'
+              placeholder='blur'
+              loading='lazy'
+            />
+            <a
+              target='_blank'
+              href='https://davidebcurtis.github.io/taskmaster-pro/'
+            >
+              <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
+                Task Master
+              </div>
+            </a>
+          </div>
+          <div className='relative absolute inset-0 cursor-pointer'>
+            <Image
+              className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
+              src={placeholder}
+              alt='Coming Soon'
+              placeholder='blur'
+              loading='lazy'
+            />
+            <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
+              Coming Soon
             </div>
           </div>
           <div className='relative absolute inset-0 cursor-pointer'>
             <Image
               className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
-              src={weather}
-              alt='Weather App'
+              src={placeholder}
+              alt='Coming Soon'
               placeholder='blur'
               loading='lazy'
             />
             <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
-              Weather App
-            </div>
-          </div>
-          <div className='relative absolute inset-0 cursor-pointer'>
-            <Image
-              className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
-              src={weather}
-              alt='Weather App'
-              placeholder='blur'
-              loading='lazy'
-            />
-            <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
-              Weather App
-            </div>
-          </div>
-          <div className='relative absolute inset-0 cursor-pointer'>
-            <Image
-              className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
-              src={weather}
-              alt='Weather App'
-              placeholder='blur'
-              loading='lazy'
-            />
-            <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
-              Weather App
-            </div>
-          </div>
-          <div className='relative absolute inset-0 cursor-pointer'>
-            <Image
-              className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
-              src={weather}
-              alt='Weather App'
-              placeholder='blur'
-              loading='lazy'
-            />
-            <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
-              Weather App
-            </div>
-          </div>
-          <div className='relative absolute inset-0 cursor-pointer'>
-            <Image
-              className='object-cover h-full max-w-full rounded-lg p-4 bg-neutral-100 bg-opacity-[.08] border-[1px] border-neutral-100'
-              src={weather}
-              alt='Weather App'
-              placeholder='blur'
-              loading='lazy'
-            />
-            <div className='bg-gradient-to-r from-primary to-secondary m-4 opacity-0 hover:opacity-70 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold'>
-              Weather App
+              Coming Soon
             </div>
           </div>
         </div>
